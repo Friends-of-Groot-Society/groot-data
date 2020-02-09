204d1b0496bfd3d00cec6cf65dfb4b0b0e1c7ca4
thomas maestas <thomasm1.maestas@gmail.com>
2020-02-09 00:13:43 -0500

    GrootRepository

--- src/main/java/net/groot/data/GrootRepository.java
+++ src/main/java/net/groot/data/GrootRepository.java
@@ -1,5 +1,6 @@
 package net.groot.data;
 
+// JpaRepository extends PagingAndSortingRepository which in turn extends CrudRepository.
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GrootRepository extends JpaRepository<Groot, Long> {
