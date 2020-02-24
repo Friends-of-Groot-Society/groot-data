e49ecd10519ea86a6a007e5f6a5e130390be236f
thomas maestas <thomasm1.maestas@gmail.com>
2020-02-24 00:52:40 -0500

    Users registration; testNG on news;

--- src/main/java/net/groot/data/controllers/UserController.java
+++ src/main/java/net/groot/data/controllers/UserController.java
@@ -3,6 +3,7 @@ package net.groot.data.controllers;
  
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
-
+ 
 import net.groot.data.entities.User;
 import net.groot.data.requests.UserRequest;
 import net.groot.data.services.UserService;
@@ -33,7 +34,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<Void> createNewUser(@Valid @RequestBody UserRequest userRequest, UriComponentsBuilder uriComponentsBuilder) {
         Long primaryKey = userService.createNewUser(userRequest);
 
@@ -43,17 +44,39 @@ public class UserController {
 
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
+    
+//	@PostMapping("/authenticate")
+//	public boolean loginCustomer(@RequestBody User user, HttpSession httpSession) {
+//		
+////		String username = httpSession.getAttribute(em).toString();
+//		
+//		if(userService.getUserByEmailAndPassword(user.getEmail(), user.getPassword()) != null) {
+//			return true;
+//		}
+//		else {
+//			return false;
+//		}
+//		
+//	} 
+//	@GetMapping( "/email/{email}")
+//    public User getUserByEmail(@PathVariable  String email) {
+//        return userService.getUserByEmail(email);
+//    }
+	@GetMapping("/email/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable("email") String email) {
+        return ResponseEntity.ok(userService.getUserByEmail(email));
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+    
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
  
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
-    }
-
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @Valid @RequestBody UserRequest userRequest) {
         return ResponseEntity.ok(userService.updateUser(id, userRequest));
